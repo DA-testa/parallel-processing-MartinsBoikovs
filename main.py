43e4c9ae6efc55a6b20b7e173d0333a1851247ab
221RDB222 <124578710+MartinsBoikovs@users.noreply.github.com>
2023-03-12 18:51:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,17 @@ def parallel_processing(n, m, data):
     return output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
+    text = input().split()
+    n = text[0]
+    m = text[1]
 
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
     data = []
+    data = input().split()
 
-    # TODO: create the function
     result = parallel_processing(n,m,data)
     
-    # TODO: print out the results, each pair in it's own line
-
-
-
+    for i,j in result:
+        print(i,j)
+    
 if __name__ == "__main__":
     main()
